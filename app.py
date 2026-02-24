118b244629014efd5c3c2faf44425959f521327c
kinai9661 <kinai9661@gmail.com>
2026-02-25 07:41:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -165,5 +165,5 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     demo.launch(server_name="0.0.0.0", server_port=port, share=False)
